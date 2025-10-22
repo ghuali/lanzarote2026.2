0c198b6a0540c1dcdc92d6392ef85871e9036543
ghuali <guaghuali@gmail.com>
2025-10-22 09:13:16 +0100

    corregido  PeliculasArray

---
+++ arrayPelisResuelto/peliculas.php
@@ -0,0 +1,89 @@
+87 % de almacenamiento usado … Si te quedas sin espacio, no podrás guardar archivos en Drive, crear copias de seguridad en Google Fotos ni usar Gmail. Disfruta de 100 GB de almacenamiento por 1,99 € 0,49 € durante 3 meses (precio personalizado).
+<?php
+$peliculas = [
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "F",
+        "poster" => "img/peliculas/esdla.png"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "C",
+        "poster" => "img/peliculas/origen.png"
+    ],
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "F",
+        "poster" => "img/peliculas/esdla.png"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "C",
+        "poster" => "img/peliculas/origen.png"
+    ],
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "F",
+        "poster" => "img/peliculas/esdla.png"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "C",
+        "poster" => "img/peliculas/origen.png"
+    ],
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "F",
+        "poster" => "img/peliculas/esdla.png"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "C",
+        "poster" => "img/peliculas/origen.png"
+    ],
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "F",
+        "poster" => "img/peliculas/esdla.png"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "C",
+        "poster" => "img/peliculas/origen.png"
+    ],
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "F",
+        "poster" => "img/peliculas/esdla.png"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "C",
+        "poster" => "img/peliculas/origen.png"
+    ]
+    // Agrega al menos 4 películas más con información similar
+];
---
+++ arraysPelis/arrays.php
@@ -0,0 +1,46 @@
+<?php
+$peliculas = [
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "Fantasía",
+        "poster" => "imagenes/lotr1.jpg"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "Ciencia Ficción",
+        "poster" => "imagenes/inception.jpg"
+    ],
+    [
+        "titulo" => "Scary Movie",
+        "director" => "Keenen Ivory Wayans",
+        "anio" => 2000,
+        "genero" => "Terror/Comedia",
+        "poster" => "imagenes/inception.jpg"
+    ],
+    [
+        "titulo" => "American Psycho",
+        "director" => "Mary Harron",
+        "anio" => 2000,
+        "genero" => "Terror/Comedia",
+        "poster" => "imagenes/americanPyscho.jpg"
+    ],
+    [
+        "titulo" => "El código Da Vinci",
+        "director" => "Ron Howard",
+        "anio" => 2006,
+        "genero" => "Suspenso/Misterio",
+        "poster" => "imagenes/inception.jpg"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "Ciencia Ficción",
+        "poster" => "imagenes/inception.jpg"
+    ],
+    
+];
